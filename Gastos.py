69714a5a597838f03b1160fdf4b47f6292fa2a4e
julian123da <julianacevedo326@gmail.com>
2025-05-16 13:29:15 -0500

    feat: :tada: begin of my proyect

---
+++ Gastos.py
@@ -0,0 +1,27 @@
+# ################################
+# ### Proyecto Python ##########
+# ################################
+
+print("=========================================")
+print("       Simulador de Gasto Diario         ")
+print("=========================================")
+print("Seleccione una opcion: ")
+print("")
+print("1.Registrar nuevo gasto")
+print("2.Lsta de gastos")
+print("3.Calcular total de gastos")
+print("4.Generar reporte de gastos")
+print("5.Salir")
+print("=========================================")
+opcionUsuario=int(input())
+
+if (opcionUsuario==1):
+    print("=====================================")
+    print("      Registar Gastos Nuevos         ")
+    print("=====================================")
+    print("")
+    print("Ingrese la informacion del gasto")
+
+    montoNuevo=input("Monto del gasto: ")
+    categoriaNueva=input("Categoria: ")
+    descripcionNueva=input("Descripcion (opcional):")
