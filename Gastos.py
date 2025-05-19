ba228f5d8f4ae003ba59f2c8054723e1d4f3713e
julian123da <julianacevedo326@gmail.com>
2025-05-19 13:13:57 -0500

    feat: :zap: Improving code

--- Gastos.py
+++ Gastos.py
@@ -8,25 +8,25 @@ from Mis_funciones import *
 from tabulate import tabulate
 import datetime
 
-listaGastos = abrir(JSON)
-
+listaGastos = abrirJSON()
 
 booleano = True
 while (booleano == True):
     menuPrincipal()
-    opcionNumerica = int(input("Selecciona tu opcion: "))
+    opcionNumeria = int(input("Selecciona tu opcion: "))
     if opcionNumerica == 1:
         primerMenu()
-        monto = input("INgresa el nombre del usuario: ")
+        monto = float(input("Ingresa el monto del gasto: "))
         categoria = input('Ingresa el apellido del usuario: ')
-        descripcion = input("(opcional Añade la descripcion de tu gusto:)")
+        descripcion = input("(opcional) Añade la descripcion de tu gasto: ")
         dic1 = {
             "id":1, "monto": monto, "categoria": categoria, "descripcion": descripcion
         }
-        listaGastos['gastos']. append(dic1)
+        listaGastos['gastos'].append(dic1)
         guardarJSON(listaGastos)
-        print (tabulate(listaGastos))
-        # table = lista
-        # print (tabulate(lista))
+        table = listaGastos
+        print(tabulate(table))
+    # table = lista
+    # print (tabulate(lista))
 
-        #Desrrollado por Julian David Acevedo Gomez TI 1.097.100.290
+    #Desrrollado por Julian David Acevedo Gomez TI 1.097.100.290
